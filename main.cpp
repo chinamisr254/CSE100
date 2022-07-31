4329e89bd490ac6344755ca9a0da316ea4e95391
Joseph Hawkins <josephhawkins1776@gmail.com>
2022-07-30 20:32:07 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  Lab 1 Question 4
+//
+//  Created by Joseph Hawkins on 5/19/22.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    // insert code here...
+    cout << "Hawkins, Joseph"<<endl;
+    cout<<33<<endl;
+    cout<<"Freshman"<<endl;
+    return 0;
+}
